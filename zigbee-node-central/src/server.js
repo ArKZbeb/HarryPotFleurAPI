0c90f30d9c0010e0761340dc098a61e75946d00c
Tristan <turgytristan@gmail.com>
2025-04-04 17:05:00 +0200

    last commit

--- zigbee-node-central/src/server.js
+++ zigbee-node-central/src/server.js
@@ -10,11 +10,10 @@ if (!process.env.SERIAL_PORT)
 
 if (!process.env.SERIAL_BAUDRATE)
   throw new Error("Missing SERIAL_BAUDRATE environment variable");
-// if (!process.env.DESTINATION_ADRESS)
-// throw new Error('Missing DESTINATION_ADRESS environment variable');
-let test = false;
+if (!process.env.DESTINATION_ADRESS)
+  throw new Error('Missing DESTINATION_ADRESS environment variable');
+let status = false;
 client.on("connect", () => {
-  client.subscribe("plante/valve", (err) => {});
   client.subscribe("plante/eau", (err) => {
     if (err) {
       console.error("Erreur lors de la souscription à plante/eau", err);
@@ -33,13 +32,12 @@ client.on("message", (topic, message) => {
     }
     if (message.toString() === "false") {
       console.log("fermeture de la valve demandée.");
-      test = true;
+      test = false;
     }
   }
 });
 
 const SERIAL_PORT = process.env.SERIAL_PORT;
-// const DESTINATION_ADRESS = process.env.DESTINATION_ADRESS;
 
 var xbeeAPI = new xbee_api.XBeeAPI({
   api_mode: 2,
@@ -61,6 +59,7 @@ serialport.pipe(xbeeAPI.parser);
 xbeeAPI.builder.pipe(serialport);
 
 const BROADCAST_ADDRESS = "FFFFFFFFFFFFFFFF";
+const DESTINATION_ADRESS =process.env.DESTINATION_ADRESS;
 serialport.on("open", function () {
   var frame_obj = {
     // AT Request to be sent
@@ -93,59 +92,29 @@ xbeeAPI.parser.on("data", function (frame) {
     console.log("C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET");
     let dataReceived = String.fromCharCode.apply(null, frame.data);
     console.log(">> ZIGBEE_RECEIVE_PACKET >", dataReceived);
-    var text = Buffer.from(frame.data).toString("hex");
-    console.log(">> ZIGBEE_RECEIVE_PACKET_FORMATED >", text);
-    const listText = text.split("/");
+    const listText = dataReceived.toString().split(",");
+    console.log(listText[0]);
     var temp = listText[0];
     var hum = listText[1];
-    var light = listText[2];
-    var data = [temp, hum, light];
-    client.publish("plante/stats", data);
+    var waterLevel = listText[2];
+    var light = listText[3];
+    var data = [temp, hum, waterLevel, light]
+    client.publish("plante/stats", JSON.stringify(data));
     console.log("Transmitted data : ", data);
   }
 
   if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
-    // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
     console.log("NODE_IDENTIFICATION");
   } else if (C.FRAME_TYPE.AT_COMMAND_RESPONSE == frame.type) {
     console.log("AT_COMMAND_RESPONSE");
   } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
     console.log("ZIGBEE_IO_DATA_SAMPLE_RX");
-    // console.log("envoyé par esteban", frame)
-    console.log(frame.digitalSamples.DIO1);
-    console.log(frame.digitalSamples.DIO2);
-    // console.log(frame.analogSamples.AD2)
-    // if(frame.digitalSamples.DIO0 === 0 ){
-    //   eteindreLampe()
-
-    /*
-      // if(frame.digitalSamples.DIO1 === 0 ){
-      //   ouvrirValve()
-
-      // }
-      // if(frame.digitalSamples.DIO2 === 0 ){
-      //   fermerValve()
-      // }
-    */
-    if (frame.analogSamples.AD0 === 0) {
-      fermerValve();
+    if (status === true) {
+      gererValve(true)
     }
-    if (frame.analogSamples.AD0 !== 0) {
-      ouvrirValve();
+    if (status === false) {
+      gererValve(false)
     }
-    if (test === true) {
-      ouvrirValve;
-    }
-    if (test === false) {
-      fermerValve;
-    }
-    // if(frame.analogSamples.AD3 = 300 && !t ){
-    //   allumerLampe()
-    // }
-
-    // if(frame.analogSamples.AD2 < 300 && t ){
-    //   eteindreLampe()
-    // }
   } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
     console.log("REMOTE_COMMAND_RESPONSE");
   } else {
@@ -156,70 +125,26 @@ xbeeAPI.parser.on("data", function (frame) {
   }
 });
 
-function ouvrirValve() {
-  console.log("Sending command to turn on the valve...");
-  const frame_obj = {
-    type: C.FRAME_TYPE.AT_COMMAND,
-    command: "D2",
-    commandParameter: [0x04],
-  };
-  xbeeAPI.builder.write(frame_obj);
-  client.publish("plante/valve", "Command sent: Turn on valve");
-  console.log("Command sent: Turn on valve");
-}
-
-function fermerValve() {
-  console.log("Sending command to turn off the valve...");
-  const frame_obj = {
-    type: C.FRAME_TYPE.AT_COMMAND,
+function gererValve(value){
+  let status = "off";
+  let frame_obj = {
+    type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+    destination64: DESTINATION_ADRESS,
     command: "D2",
     commandParameter: [0x05],
   };
+  if(value = true){
+    status = "on";
+    frame_obj = {
+      type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+      destination64: DESTINATION_ADRESS,
+      command: "D2",
+      commandParameter: [0x04],
+    };
+  }
+
+  console.log("Sending command to turn ", {status}, " the valve...");
   xbeeAPI.builder.write(frame_obj);
-  client.publish("plante/valve", "Command sent: Turn off valve");
-  console.log("Command sent: Turn off valve");
+  client.publish("plante/valve", "Command sent: Turn ", {status}, " valve");
+  console.log("Command sent: Turn ", {status}, " valve");
 }
-
-// function getStatusLampe(){
-//   console.log("Getting status of light");
-//   const frame_obj = {
-//     type: C.FRAME_TYPE.AT_COMMAND,
-//     command: "D0",
-//     commandParameter: [],
-//   };
-//   xbeeAPI.builder.write(frame_obj);
-//   return true
-// }
-
-// function testEnvoie(){
-//   console.log("envoie");
-//   const frame_obj = {
-//     type: C.FRAME_TYPE.ZIGBEE_TRANSMIT_REQUEST,
-//     destination64: test,
-//     data: "TxData0A"
-
-//   };
-//   xbeeAPI.builder.write(frame_obj);
-// }
-
-// function eteindreLampe() {
-//   console.log("Sending command to turn off the lamp...");
-//   const frame_obj = {
-//     type: C.FRAME_TYPE.AT_COMMAND,
-//     command: "D0",
-//     commandParameter: [0x04],
-//   };
-//   xbeeAPI.builder.write(frame_obj);
-//   console.log("Command sent: Turn off lamp");
-// }
-
-// function allumerLampe() {
-//   console.log("Sending command to turn on the lamp...");
-//   const frame_obj = {
-//     type: C.FRAME_TYPE.AT_COMMAND,
-//     command: "D0",
-//     commandParameter: [0x05],
-//   };
-//   xbeeAPI.builder.write(frame_obj);
-//   console.log("Command sent: Turn on lamp");
-// }
